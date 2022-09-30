0c8640f7ad3a40a09f4d6164cb6e6930d375291a
RishabhRai <rrishabhrai19105@gmail.com>
2022-09-30 14:02:14 +0530

    New Add

--- scripts/bag.js
+++ scripts/bag.js
@@ -1,1 +1,16 @@
 import { Bag_Nav } from "./bag_navbar.js";
+
+let show_btn = document.querySelector("#show_btn");
+let showMore = document.querySelector("#showMore")
+
+show_btn.addEventListener("click" , function(){
+    showMore.style.display = "block";
+    show_btn.style.display = "none";
+})
+
+let hide_btn = document.querySelector("#hide_btn");
+
+hide_btn.addEventListener("click" , function(){
+    showMore.style.display = "none";
+    show_btn.style.display = "block"   
+})
