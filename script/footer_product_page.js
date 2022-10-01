42059fe154ee2db42a20cc27f9d99be541d79ab2
Amitk2108 <ak153603@gmail.com>
2022-10-01 21:46:46 +0530

    Update

---
+++ script/footer_product_page.js
@@ -0,0 +1,181 @@
+function footer() {
+  return `
+    <footer>
+    <div id="main-container">
+      <div id="container1">
+        <div id="container2">
+          <h5><p style="color: black">ONLINE SHOPPING</p></h5>
+          <!-- <span>ONLINE SHOPPING</span> -->
+          <p><span>Men</span></p>
+          <p><span>Women</span></p>
+          <p><span>Kids</span></p>
+          <p><span>Home</span></p>
+          <p><span>Beauty</span></p>
+          <p><span>Gift Cards</span></p>
+          <p><span>Myntra Insider</span></p>
+          <h5>
+            <p><span style="color: black">USEFUL LINKS</span></p>
+          </h5>
+          <p><span>Blog</span></p>
+          <p><span>Careers</span></p>
+          <p><span>Site Map</span></p>
+          <p><span>Corporate Information</span></p>
+          <p><span>Whitehat</span></p>
+        </div>
+
+        <div id="container3">
+          <h5><p style="color: black">CUSTOMER POLICIES</p></h5>
+
+          <p><span>Contact Us</span></p>
+          <p><span>FAQ</span></p>
+          <p><span>T&amp;C</span></p>
+          <p><span>Terms Of Use</span></p>
+          <p><span>Track Orders</span></p>
+          <p><span>Shipping</span></p>
+          <p><span>Cancellation</span></p>
+          <p><span>Returns</span></p>
+          <p><span>Privacy policy</span></p>
+          <p><span>Grievance Officer</span></p>
+        </div>
+
+        <div id="container4">
+          <h5><p style="color: black">EXPERIENCE MYNTRA APP ON MOBILE</p></h5>
+          <p>
+            <img
+              style="width: 150px; height: 49px"
+              src="https://constant.myntassets.com/web/assets/img/80cc455a-92d2-4b5c-a038-7da0d92af33f1539674178924-google_play.png"
+              alt=""
+            />
+          </p>
+          <p>
+            <img
+              style="width: 150px; height: 49px"
+              src="https://constant.myntassets.com/web/assets/img/bc5e11ad-0250-420a-ac71-115a57ca35d51539674178941-apple_store.png"
+              alt=""
+            />
+          </p>
+          <h6><p style="color: black">KEEP IN TOUCH</p></h6>
+          <p>
+            <img
+              style="width: 30px; height: 30px"
+              src="https://constant.myntassets.com/web/assets/img/d2bec182-bef5-4fab-ade0-034d21ec82e31574604275433-fb.png"
+              alt=""
+            />
+          </p>
+          <p>
+            <img
+              style="width: 30px; height: 30px"
+              src="https://constant.myntassets.com/web/assets/img/f10bc513-c5a4-490c-9a9c-eb7a3cc8252b1574604275383-twitter.png"
+              alt=""
+            />
+          </p>
+          <p>
+            <img
+              style="width: 30px; height: 30px"
+              src="https://constant.myntassets.com/web/assets/img/b4fcca19-5fc1-4199-93ca-4cae3210ef7f1574604275408-insta.png"
+              alt=""
+            />
+          </p>
+        </div>
+
+        <div id="container5">
+          <div>
+            <div>
+              <div>
+                <img
+                  src="https://constant.myntassets.com/web/assets/img/6c3306ca-1efa-4a27-8769-3b69d16948741574602902452-original.png"
+                  style="width: 48px; height: 40px"
+                />
+              </div>
+              <div>
+                <strong>100% ORIGINAL </strong
+                ><!--  -->
+                guarantee for all products at myntra.com
+                <!-- / -->
+              </div>
+            </div>
+            <div>
+              <div>
+                <img
+                  src="https://constant.myntassets.com/web/assets/img/ef05d6ec-950a-4d01-bbfa-e8e5af80ffe31574602902427-30days.png"
+                  style="width: 48px; height: 49px"
+                />
+              </div>
+              <div>
+                <strong>Return within 30days </strong
+                ><!--  -->of receiving your order
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div id="container6">
+        <div>
+          <div>
+            <div><hr /></div>
+            <div>
+              <h4>POPULAR SEARCHES</h4>
+            </div>
+            <div id="psearch">
+              Makeup | Dresses For Girls | T-Shirts | Sandals | Headphones |
+              Babydolls | Blazers For Men | Handbags | Ladies Watches | Bags |
+              Sport Shoes | Reebok Shoes | Puma Shoes | Boxers | Wallets |
+              Tops | Earrings | Fastrack Watches | Kurtis | Nike | Smart
+              Watches | Titan Watches | Designer Blouse | Gowns | Rings |
+              Cricket Shoes | Forever 21 | Eye Makeup | Photo Frames | Punjabi
+              Suits | Bikini | Myntra Fashion Show | Lipstick | Saree |
+              Watches | Dresses | Lehenga | Nike Shoes | Goggles | Bras | Suit
+              | Chinos | Shoes | Adidas Shoes | Woodland Shoes | Jewellery |
+              Designers Sarees
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div id="container7">
+        <div>
+          <div>
+            In case of any concern
+            <span style="color: blue; font-weight: bold; font-size: 16px"></span>
+              , Contact Us</span
+            >
+          </div>
+          <div>© 2022 www.myntra.com. All rights reserved.</div>
+        </div>
+      </div>
+
+      <span><hr /> </span>
+
+      <div id="container8">
+        <address>
+          <h3 style="color: black">Registered Office Address</h3>
+
+          <div>
+            <div>
+              <!-- .:  -->Buildings Alyssa,
+              <!-- /. --><br /><!-- .:  -->Begonia and Clover situated in
+              Embassy Tech Village,
+              <!-- /. --><br /><!-- .:  -->Outer Ring Road,
+              <!-- /. --><br /><!-- .:  -->Devarabeesanahalli Village,
+              <!-- /. --><br /><!-- .:  -->Varthur Hobli,
+              <!-- /. --><br /><!-- .:  -->Bengaluru – 560103, India
+              <!-- /. -->
+            </div>
+            <div id="addph">
+              <p>
+                CIN: U72300KA2007PTC041799 <br />
+
+                Telephone:
+                <!-- /. --><a href="tel:+918061561999">+91-80-61561999</a>
+              </p>
+            </div>
+          </div>
+        </address>
+      </div>
+    </div>
+  </footer>
+    
+    `;
+}
+export { footer };
