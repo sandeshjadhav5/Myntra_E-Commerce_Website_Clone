ba7ce9d449b8bee1e818a287f0d7d03751c033b6
RishabhRai <rrishabhrai19105@gmail.com>
2022-10-02 14:40:03 +0530

    Add

--- scripts/address.js
+++ scripts/address.js
@@ -1,1 +1,30 @@
 import { Bag_Nav } from "./bag_navbar.js";
+
+function addAddress() {
+      const form = document.getElementById("addressForm");
+  
+      const name = form.name.value;
+      const mobileNo = form.mobileNo.value;
+      const pincode = form.pincode.value;
+      const address = form.address.value;
+      const location = form.location.value;
+      const city = form.data2.value;
+      const state = form.data1.value;
+  
+      let address1 = {
+          name,
+          mobileNo,
+          pincode,
+          address,
+          location,
+          city,
+          state
+      };
+      for (k in address1) {
+          if (address1[k].length === 0) {
+              alert("Please fill all feilds")
+              return;
+          }
+      }
+      window.location.href = "payment.html"
+  }
